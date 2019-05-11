d2ad818e896844041691ffeb0bb6a150a734f9a8
Alex <43788503+BionicBarry@users.noreply.github.com>
2019-05-11 10:37:57 -0400

    Update Alerts.java

--- Alerts.java
+++ Alerts.java
@@ -1,63 +1,66 @@
-package com.example.samplechatapplication.utils;
+package com.example.bionicbarry.smorse;
+//Class that tells the device to vibrate
+import android.os.Vibrator;
 
-import android.content.Context;
-
-public class Alerts {
+public class Alerts{
     //Morse Translate class
     MorseTranslate translator = new MorseTranslate();
-
+    
     //Static vibrations patterns for urgent alerts
     final long[] SOS = translator.convertToMorse("sos");
     final long[] ATN = translator.convertToMorse("attn");
-    //User Settings placed here because the Alerts class interacts with the
+    //User Settings placed here because the Alerts class interacts with the 
     //User setting class the most
-    Context mContext;
+    UserSettings settings = new UserSettings();
+    //Added Vibrator Object for proper functionality
+    Vibrator viber = new Vibrator();
 
-    public Alerts(Context mContext) {
-        this.mContext = mContext;
+
+    public Alerts(){
+        
     }
-
-    //switch method used for varying levels of urgency.
-    public void findUrgency(int urg, String message) {
+    
+    //switch method used for varying levels of urgency.  
+    public void findUrgency(int urg, String message){
         //Changed the boolean values for the flash and vibrate morse relays
         //to avoid encapsulation, and centralize the user settings
-        boolean vib = SharePreferenceData.INSTANCE.getBooleanPreference(mContext, UtilClass.INSTANCE.getSWITCH_VIBRATION(), false);
-        boolean flash = SharePreferenceData.INSTANCE.getBooleanPreference(mContext, UtilClass.INSTANCE.getSWITCH_FLAG(), false);
-
-        switch (urg) {
-            //Standard Message
-            case 0:
-                morseRelay(vib, flash, "");
-                break;
-            //Urgent Message
-            case 1:
-                // UtilClass.INSTANCE.vibrateNow(mContext,ATN);//vibrate(ATN);
-                morseRelay(vib, flash, "sos");
-                break;
-            //Emergency Message
-            case 2:
-                // UtilClass.INSTANCE.vibrateNow(mContext,SOS);//vibrate(SOS);
-                morseRelay(vib, flash, "attn");
-                break;
-
+        boolean vib = settings.getFlash();
+        boolean flash = settings.getFlash();
+        
+        switch(urg){
+                //Standard Message
+                case 0:
+                    morseRelay(vib, flash, message);
+                    break;
+                //Urgent Message
+                case 1:
+                    viber.vibrate(ATN, 0);
+                    morseRelay(vib, flash, message);
+                    break;
+                //Emergency Message
+                case 2:
+                    viber.vibrate(SOS, 0);
+                    morseRelay(vib, flash, message);
+                    break;
+                    
         }
     }
 
     //method used for checking user preferences for morse relay settings
-    private void morseRelay(boolean vib, boolean flash, String message) {
+    private void morseRelay(boolean vib, boolean flash, String message){
 
         //if vibrate user setting is active
-        if (vib && flash) {
-            UtilClass.INSTANCE.vibrateNow(mContext, translator.convertToMorse(message));
-            UtilClass.INSTANCE.flashLightPattern(mContext,translator.convertToMorseTorch(message));
-            //if neither morse relay user setting is active
-        } else if (vib) {
-            UtilClass.INSTANCE.vibrateNow(mContext, translator.convertToMorse(message));//vibrate(translator.convertToMorse(message));
-            //if flash user setting is active
-        } else if (flash) {
-            UtilClass.INSTANCE.flashLightPattern(mContext,translator.convertToMorseTorch(message));
-            //if flash and vibrate user setting is active
-        } else {
+        if(vib){
+            viber.vibrate(translator.convertToMorse(message),0);
+        //if flash user setting is active
+        }else if(flash){
+            flashlight(translator.convertToMorse(message),0);
+        //if flash and vibrate user setting is active
+        }else if (vib && flash){
+            viber.vibrate(translator.convertToMorse(message),0);
+            flashlight(translator.convertToMorse(message),0);
+        //if neither morse relay user setting is active
+        }else{
             return;
         }
     }
